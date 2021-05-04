ce1aa167708a92e350daa8678826dcc9c66e5e0a
Андрей <aspashkov87@gmail.com>
2021-05-04 14:23:05 +0300

    Add Class

---
+++ VK/Model/MyGroupsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyGroupsTableViewCell.swift
+//  VK
+//
+//  Created by Андрей Пашков on 04.05.2021.
+//
+
+import UIKit
+
+class MyGroupsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
